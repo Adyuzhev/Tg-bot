737b619bdbd7efb0f61ab7f117585d53112d8874
Alexey Dyuzhev <LESHA-31@mail.ru>
2022-10-19 20:55:42 +0500

    model fasttext_tfidf_autofaq

---
+++ new.py
@@ -0,0 +1,11 @@
+from deeppavlov import build_model, train_model
+
+
+model = train_model('./copy.json')
+
+model = build_model('./copy.json')
+
+
+result = model(['Можно ли сомещать обучение в магистратуре с работой?'])
+
+print(result)
