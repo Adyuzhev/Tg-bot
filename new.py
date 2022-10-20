bbc7e63c7dc74914c89fe260e4b8cd3799199bc9
desretoot <matticus.diamond@mail.ru>
2022-10-20 16:13:31 +0500

    rename

--- new.py
+++ new.py
@@ -1,8 +1,8 @@
 from deeppavlov import build_model, train_model
 
-model = train_model('./copy.json')
+model = train_model('./fasttext_tfidf_autofaq.json')
 
-model = build_model('./copy.json')
+model = build_model('./fasttext_tfidf_autofaq.json')
 
 
 result = model(['Можно ли сомещать обучение в магистратуре с работой?'])
