a0da9210029709786407a5251b5f47f398709434
Alexey Dyuzhev <LESHA-31@mail.ru>
2022-11-26 00:29:06 +0500

    delete duplicates

--- new.py
+++ new.py
@@ -1,14 +1,22 @@
 from deeppavlov import build_model, train_model
 
-# model = train_model('./tfidf_autofaq.json')
+model = train_model('./tfidf_autofaq.json')
 
 model = build_model('./tfidf_autofaq.json')
+sum = 0
+with open('FAQ.csv') as f:
+    f.readline()
+    for line in f:
+        a = line.split(',')[0]
+        result = model([a])
+        if result[1][0] <= 0.04:
+            print(a, result)
+            sum += 1
+    print(sum)
 
+result1 = model(['Могу ли я поступить на направление магистратуры, которое никак не связано с образованием, полученным на бакалавриате?'])
 
-
-result = model(['Сколько раз в неделю нужно приезжать в университет студентам магистратуры?'])
-
-if result[1][0] <= 0.1:
+if result[1][0] <= 0.04:
     print('Уточните вопрос')
 else:
-    print(result)
+    print(result1)
